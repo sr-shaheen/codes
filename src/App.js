9ee726657da25381f5e624af05a7880a849d2e06
sr-shaheen <shaheenfgs@gmail.com>
2024-07-26 11:42:02 +0600

    feat: custom hooks

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { checkProps } from "./hocs/check-props";
+import { includeUser } from "./hocs/include-user";
+import { UserInfoForm } from "./hocs/user-form";
+import { UserInfo } from "./hocs/user-info";
+
+function App() {
+  return (
+    <>
+      <UserInfoForm />
+    </>
+  );
+}
+
+export default App;
