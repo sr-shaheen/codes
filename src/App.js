1f8b02ef790c165cbe80db03a40fb6f2b6119608
sr-shaheen <shaheenfgs@gmail.com>
2024-07-26 16:14:36 +0600

    feat: compound components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { checkProps } from "./hocs/check-props";
 import { includeUser } from "./hocs/include-user";
-import { UserInfoForm } from "./hocs/user-form";
+import { UserInfoForm } from "./components/hocs/user-form";
 import { UserInfo } from "./hocs/user-info";
 
 function App() {
